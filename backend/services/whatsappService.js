0ba540f42ade86062038fea715664a468380c4a7
Abhi <labprograms225@gmail.com>
2026-08-19 12:42:39 +0530

    Fix 'cant link new devices' error by removing hardcoded webVersionCache

--- backend/services/whatsappService.js
+++ backend/services/whatsappService.js
@@ -18,10 +18,6 @@ const initWhatsApp = async () => {
                 store: store,
                 backupSyncIntervalMs: 300000
             }),
-            webVersionCache: {
-                type: 'remote',
-                remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-            },
             puppeteer: {
                 args: ['--no-sandbox', '--disable-setuid-sandbox'],
                 executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
